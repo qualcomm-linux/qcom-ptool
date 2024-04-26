91a0cd071ea31e316b2fee2c12028e8ea1a18ba6
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2024-04-26 21:22:47 +0300

    degpt: add tool to parse gpt tables back to partition.xml
    Import a tool to reparse gpt_bothN.bin tables into the partition.xml
    format. This is useful to restore the partition schema basing on binary
    files from the fastboot flashing package.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

---
+++ degpt/src/mygpt.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024 Linaro Ltd.
+
+pub mod header;
+pub mod partition;
+
+pub use header::Header;
+pub use partition::Partition;
+pub use partition::Type;
